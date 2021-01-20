72d6cf57784b14402e03e21e2e428ee149cf6b7a
gustav@virtuelldesign.se <gustav@virtuelldesign.se>
2021-01-20 11:55:05 +0100

    Now hides measure button in AR on iOS
    It checks if in AR and if on IOS and hides measure if true

--- ReflectViewer/Assets/Scripts/HideInAR.cs
+++ ReflectViewer/Assets/Scripts/HideInAR.cs
@@ -1,10 +1,11 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using Unity.Reflect.Viewer.UI;
 using UnityEngine;
 
 public class HideInAR : MonoBehaviour
 {
+    public bool onlyOnIOS = false;
     public List<GameObject> objects;
     // Start is called before the first frame update
     void Start()
@@ -19,9 +20,20 @@ public class HideInAR : MonoBehaviour
     {
         if (UIStateManager.current.stateData.navigationState.navigationMode != lastFrameNavigation && UIStateManager.current.stateData.navigationState.navigationMode == NavigationMode.AR)
         { // In AR
-            foreach (GameObject go in objects)
+            bool doHide = true;
+            if (onlyOnIOS)
             {
-                go.SetActive(false);
+                doHide = false;
+#if UNITY_IOS
+                doHide = true;
+#endif
+            }
+            if (doHide)
+            {
+                foreach (GameObject go in objects)
+                {
+                    go.SetActive(false);
+                }
             }
         } else if (UIStateManager.current.stateData.navigationState.navigationMode != lastFrameNavigation && UIStateManager.current.stateData.navigationState.navigationMode != NavigationMode.AR)
         {
